07887125efd5be59214e5ba0e12667145b3f3611
lalbona <lavanyake504@gmail.com>
2019-06-17 13:24:01 +0000

    Modified web api POST call to write data to disk.

--- cvo/checkouts/simpleweb.py
+++ cvo/checkouts/simpleweb.py
@@ -1,6 +1,7 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
-import os;
+import os
+from urlparse import urlparse, parse_qs
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -10,14 +11,25 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
+
         self.wfile.write("".join(["<html><body><h1>Reader</h1><h2>Date :", os.popen("date").read(), "</h2></body></html>"]))
         self.wfile.write("".join(["<html><body><h4>Mounts:", os.popen("mount | egrep -o '(^.*?\:.*? on .*? type nfs.)'").read(), "/<h4>",  "<h2>CWD :", os.popen("pwd").read(), "</h2><pre>", os.popen("ls -ltr").read(), "</pre></body></html>"]))
 
     def do_POST(self):
         # Doesn't do anything with posted data
+        o = urlparse(self.path)
+        qmap = parse_qs(o[4])
+        fname = qmap['fname'][0]
+        fsize = qmap['fsize'][0]
+        tfile = "".join(['/opt/cvo/', fname, '.txt'])
+
+        f = open(tfile, "w")
+        f.write(str("P" * int(fsize)))
+        f.close()
+
         self._set_headers()
-        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
-        
+        self.wfile.write("<html><body><h1>POST!</h1><p>Data written</p></body></html>")
+
 def run(server_class=HTTPServer, handler_class=S, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
